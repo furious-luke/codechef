473b46f708355fe18f1c827c563f67f6771f36b9
Luke Hodkinson <furious.luke@gmail.com>
2014-04-28 14:56:55 +1000

    Nearly have Crocodile working. Still have a couple of issues in there I need to debug.

--- Practice (easy)/Brush Fire/main.cc
+++ Practice (easy)/Brush Fire/main.cc
@@ -2,60 +2,6 @@
 #include <cstdio>
 #include <algorithm>
 
-// class fast_atoi
-// {
-// public:
-
-//    fast_atoi()
-//       : _value( 0 )
-//    {
-//    }
-
-//    inline
-//    int
-//    value() const
-//    {
-//       return _value;
-//    }
-
-//    inline
-//    bool
-//    operator()( std::pair<char const*,size_t> const& str )
-//    {
-//       (*this)( str.first, str.second );
-//    }
-
-//    inline
-//    bool
-//    operator()( char const* str,
-//                size_t len )
-//    {
-//       _value = 0;
-
-//       switch( len )
-//       {
-//          case 10:    _value += (str[len - 10] - '0')*1000000000;
-//          case  9:    _value += (str[len -  9] - '0')*100000000;
-//          case  8:    _value += (str[len -  8] - '0')*10000000;
-//          case  7:    _value += (str[len -  7] - '0')*1000000;
-//          case  6:    _value += (str[len -  6] - '0')*100000;
-//          case  5:    _value += (str[len -  5] - '0')*10000;
-//          case  4:    _value += (str[len -  4] - '0')*1000;
-//          case  3:    _value += (str[len -  3] - '0')*100;
-//          case  2:    _value += (str[len -  2] - '0')*10;
-//          case  1:    _value += (str[len -  1] - '0');
-//             return _value > 0;
-
-//          default:
-//             return false;
-//       }
-//    }
-
-// private:
-
-//    int _value;
-// };
-
 class tokenizer
 {
 public:
--- Practice (school)/Crocodile/main.cc
+++ Practice (school)/Crocodile/main.cc
@@ -9,15 +9,95 @@
 #include <cstdlib>
 #include <cstdint>
 #include <iostream>
-#include <map>
 #include <vector>
 #include <array>
 #include <queue>
 #include <limits>
 
+class tokenizer
+{
+public:
+
+   static const unsigned max_buffer_size = 20000;
+   static const unsigned default_limit = 20;
+
+public:
+
+   tokenizer()
+      : _pos( _buf + max_buffer_size ),
+        _limit( default_limit )
+   {
+   }
+
+   // Hardcoding to just return an int to avoid
+   // repetition.
+   inline
+   int
+   // std::pair<char const*,size_t>
+   next()
+   {
+      refill();
+      while( std::isspace( *_pos ) )
+         ++_pos;
+      // char* start = _pos;
+      // while( !std::isspace( *_pos ) )
+      //    ++_pos;
+      // return std::make_pair( start, _pos - start );
+      int val = *_pos++ - '0';
+      while( std::isdigit( *_pos ) )
+         val = 10*val + *_pos++ - '0';
+      return val;
+   }
+
+   inline
+   void
+   refill()
+   {
+      if( !_done )
+      {
+         size_t rem = _buf + max_buffer_size - _pos;
+         if( rem < _limit )
+         {
+            std::copy( _pos, _buf + max_buffer_size, _buf );
+            auto size = std::fread( _buf + rem, sizeof(char), sizeof(_buf) - rem, stdin );
+            _pos = _buf;
+            if( !size )
+               _done = true;
+         }
+      }
+   }
+
+protected:
+
+   char _buf[max_buffer_size];
+   char* _pos;
+   bool _done = false;
+   size_t _limit;
+};
+
+// ///
+// /// Edges need to be unidirectional, always pointing from
+// /// lower indexed vertex to higher.
+// ///
+// template< class T,
+//           class DisplType >
+// void
+// edges_to_adjacency( tokenizer& tok,
+//                     std::vector<T>& data,
+//                     std::vector<DisplType>& displs )
+// {
+//    std::sort( ((std::vector<std::array<T,2> >&)data).begin(), ((std::vector<std::array<T,2>>)data).end(),
+//               []( std::array<T,2> const& a,
+//                   std::array<T,2> const& b )
+//               {
+//                  return a[0] < b[0];
+//               } );
+//    // std::qsort( adj.data(), M, 2*sizeof(uint16_t), cmp );
+// }
+
 struct Q_cmp
 {
-   Q_cmp( std::vector<std::array<uint32_t,2> > const& dist )
+   Q_cmp( std::vector<std::array<std::array<uint32_t,2>,2> > const& dist )
       : dist( dist )
    {
    }
@@ -26,53 +106,68 @@ struct Q_cmp
    operator()( uint32_t a,
                uint32_t b ) const
    {
-      return dist[a][1] > dist[b][1];
+      return dist[a][1][0] > dist[b][1][0];
    }
 
-   std::vector<std::array<uint32_t,2> > const& dist;
+   std::vector<std::array<std::array<uint32_t,2>,2> > const& dist;
 };
 
 void
 dijkstra( std::vector<uint32_t> const& exits,
-          std::multimap<uint32_t,std::pair<uint32_t,uint32_t> > const& adj,
-          std::vector<std::array<uint32_t,2> >& dist )
+          std::vector<uint32_t> const& displs,
+          std::vector<std::array<uint32_t,2>> const& adj,
+          std::vector<std::array<std::array<uint32_t,2>,2> >& dist )
 {
    std::priority_queue<uint32_t,std::vector<uint32_t>,Q_cmp> Q{ Q_cmp( dist ) };
    for( uint32_t ii = 0; ii < dist.size(); ++ii )
-      dist[ii][0] = dist[11][1] = std::numeric_limits<uint32_t>::max();
+   {
+      dist[ii][0][0] = dist[ii][0][1] = std::numeric_limits<uint32_t>::max();
+      dist[ii][1][0] = dist[ii][1][1] = std::numeric_limits<uint32_t>::max();
+   }
    for( uint32_t ii = 0; ii < exits.size(); ++ii )
    {
-      dist[exits[ii]][0] = dist[exits[ii]][1] = 0;
+      dist[exits[ii]][0][0] = dist[exits[ii]][1][0] = 0;
+      dist[exits[ii]][0][1] = dist[exits[ii]][1][1] = exits[ii];
       Q.push( exits[ii] );
    }
    while( !Q.empty() )
    {
       uint32_t u = Q.top();
       Q.pop();
-      // std::cout << "u: " << u << "\n";
-      auto rng = adj.equal_range( u );
-      while( rng.first != rng.second )
+      // std::cout << "u: " << u << "," << dist[u][0][0] << "," << dist[u][1][0] << "\n";
+      for( uint32_t ii = displs[u]; ii < displs[u + 1]; ++ii )
       {
-         uint32_t v = rng.first->second.first;
-         uint32_t w = rng.first->second.second;
-         // std::cout << "  v: " << v << "\n";
-         // std::cout << "    d[u][1] + w: " << dist[u][1] + w << "\n";
-         // std::cout << "    d[v][0]    : " << dist[v][0] << "\n";
+         uint32_t v = adj[ii][0];
+         uint32_t w = adj[ii][1];
+         // std::cout << "  v: " << v << "," << w << "\n";
+         // std::cout << "    BEFORE\n";
+         // std::cout << "      d[v][0]: " << dist[v][0][0] << "\n";
+         // std::cout << "      d[v][1]: " << dist[v][1][0] << "\n";
          bool reheap = false;
-         if( dist[u][1] + w < dist[v][0] )
+         if( dist[u][1][0] + w < dist[v][0][0] )
          {
-            dist[v][1] = dist[v][0];
-            dist[v][0] = dist[u][0] + w;
+            if( dist[v][0][0] != u )
+            {
+               dist[v][1] = dist[v][0];
+               std::cout << dist[v][1][0] << "\n";
+            }
+            dist[v][0][0] = dist[u][1][0] + w;
+            dist[v][0][1] = u;
             reheap = true;
          }
-         else if( dist[u][1] + w < dist[v][1] )
+         else if( dist[u][1][0] + w < dist[v][1][0] &&
+                  dist[v][0][1] != u )
          {
-            dist[v][1] = dist[u][1] + w;
+            dist[v][1][0] = dist[u][1][0] + w;
+            dist[v][1][1] = u;
+            std::cout << dist[v][1][0] << "\n";
             reheap = true;
          }
          if( reheap )
             Q.push( v );
-         ++rng.first;
+         // std::cout << "    AFTER\n";
+         // std::cout << "      d[v][0]: " << dist[v][0][0] << "\n";
+         // std::cout << "      d[v][1]: " << dist[v][1][0] << "\n";
       }
    }
 }
@@ -81,22 +176,44 @@ int
 main()
 {
    unsigned N, M, K;
-   std::cin >> N >> M >> K;
-   std::cout << N << ", " << M << ", " << K << "\n";
-   std::multimap<uint32_t,std::pair<uint32_t,uint32_t> > adj;
-   std::vector<uint32_t> exits( K );
-   std::vector<std::array<uint32_t,2> > dist( N );
-   for( uint32_t ii = 0; ii < M; ++ii )
+   std::vector<uint32_t> displs, exits;
+   std::vector<std::array<uint32_t,2>> adj;
+   std::vector<std::array<std::array<uint32_t,2>,2> > dist;
    {
-      uint32_t u, v;
-      uint32_t l;
-      std::cin >> u >> v >> l;
-      adj.emplace( u, std::make_pair( v, l ) );
-      adj.emplace( v, std::make_pair( u, l ) );
+      tokenizer token;
+      N = token.next();
+      M = token.next();
+      K = token.next();
+      exits.resize( K );
+      dist.resize( N );
+      displs.resize( N + 1 );
+      adj.resize( 2*M );
+      std::vector<std::array<uint32_t,2>> edges( M );
+      std::vector<uint32_t> costs( M );
+      std::fill( displs.begin(), displs.end(), 0 );
+      for( uint32_t ii = 0; ii < M; ++ii )
+      {
+         edges[ii][0] = token.next(); edges[ii][1] = token.next();
+         costs[ii] = token.next();
+         ++displs[edges[ii][0] + 1];
+         ++displs[edges[ii][1] + 1];
+      }
+      for( uint32_t ii = 0; ii < K; ++ii )
+         exits[ii] = token.next();
+      for( uint32_t ii = 1; ii <= N; ++ii )
+         displs[ii] += displs[ii - 1];
+      std::vector<uint16_t> cnts( N );
+      std::fill( cnts.begin(), cnts.end(), 0 );
+      for( uint32_t ii = 0; ii < M; ++ii )
+      {
+         uint32_t u = edges[ii][0], v = edges[ii][1];
+         uint32_t c = costs[ii];
+         uint32_t d = displs[u] + cnts[u]++;
+         adj[d][0] = v; adj[d][1] = c;
+         d = displs[v] + cnts[v]++;
+         adj[d][0] = u; adj[d][1] = c;
+      }
    }
-   for( uint32_t ii = 0; ii < K; ++ii )
-      std::cin >> exits[ii];
-   // std::sort( exits.begin(), exits.end() );
 
    // // Remove all 2-edge rooms.
    // {
@@ -116,7 +233,7 @@ main()
    //       adj.erase( rem );
    // }
 
-   dijkstra( exits, adj, dist );
+   dijkstra( exits, displs, adj, dist );
 
-   std::cout << dist[0][1] << "\n";
+   std::cout << dist[0][1][0] << "\n";
 }
